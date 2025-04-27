be10e92c0ae9752f9bc4e7f7ad5e6f1de6a364e3
S Anand <root.node@gmail.com>
2025-04-27 21:12:08 +0800

    FIX: gcalcli shows 2 day agenda

--- setup.fish
+++ setup.fish
@@ -37,7 +37,7 @@ export PAGER='bat'      # Required for cmdg
 export EDITOR='nano'    # Required for cmdg
 abbr --add mail cmdg
 abbr --add gcalcli 'uvx gcalcli'
-abbr --add agenda 'uvx gcalcli agenda --calendar $EMAIL --nodeclined $(date -Ihours) (date -I --date tomorrow)'
+abbr --add agenda 'uvx gcalcli agenda --calendar $EMAIL --nodeclined $(date -Ihours) (date -I --date "+2 days")'
 
 # Utilities and configurations
 abbr --add clip 'xclip -selection clipboard'
