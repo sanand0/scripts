3afb5026a40a3b7f6ccf289bed54bc3975ea54e8
S Anand <root.node@gmail.com>
2025-11-16 12:48:15 +0000

    Nov 16, 2025, 8:48 PM

--- services/setup.sh
+++ services/setup.sh
@@ -53,8 +53,7 @@ fi
 # journalctl --user --since $(date -I  --date="1 week ago") -u $SERVICE
 
 # Disable services by masking them
-# systemctl --user stop $SERVICE
-# systemctl --user mask $SERVICE
+# SERVICE=... systemctl --user stop $SERVICE; systemctl --user disable $SERVICE; systemctl --user mask $SERVICE
 
 # Re-enable services by unmasking them
 # systemctl --user unmask $SERVICE
